53041035d3e1b38be24c6e6b30abbafbb77384c4
hhmadan <hhmadan03@gmail.com>
2022-10-08 18:10:35 +0530

    ADDED: Dice Roll Program

--- src/com/snakeandladder/SnakeAndLadderProblem.java
+++ src/com/snakeandladder/SnakeAndLadderProblem.java
@@ -1,7 +1,12 @@
+//The Player rolls the die to get a number between 1 - 6...
 package com.snakeandladder;
 
 public class SnakeAndLadderProblem {
     public static void main(String[] args) {
-        System.out.println("Welcome to Snake And Ladder Program");
+        int dice_roll = ((int) (Math.random()*(6))+1);
+//        Random rnd = new Random();
+//        int dice_roll = rnd.nextInt(1, 7);
+
+                System.out.println("Dice Number is: "+dice_roll);
     }
 }
