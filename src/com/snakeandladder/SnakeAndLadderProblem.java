cf22d26db2e488bd7b5091a64ac94843f918b292
hhmadan <hhmadan03@gmail.com>
2022-10-08 18:03:58 +0530

    Displaying Welcome Message

---
+++ src/com/snakeandladder/SnakeAndLadderProblem.java
@@ -0,0 +1,7 @@
+package com.snakeandladder;
+
+public class SnakeAndLadderProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake And Ladder Program");
+    }
+}
