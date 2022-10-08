a97639ff170994b2069d65efed49f868ee44ef18
hhmadan <hhmadan03@gmail.com>
2022-10-08 18:16:28 +0530

    ADDED: Start Position Program

--- src/com/snakeandladder/SnakeAndLadderProblem.java
+++ src/com/snakeandladder/SnakeAndLadderProblem.java
@@ -3,10 +3,7 @@ package com.snakeandladder;
 
 public class SnakeAndLadderProblem {
     public static void main(String[] args) {
-        int dice_roll = ((int) (Math.random()*(6))+1);
-//        Random rnd = new Random();
-//        int dice_roll = rnd.nextInt(1, 7);
-
-                System.out.println("Dice Number is: "+dice_roll);
+        int start_Position = 0;
+        System.out.println("Starting position of Player is: "+start_Position);
     }
 }
