27d87fe759f0d515087e9955285c4f47eceaf3e9
hhmadan <hhmadan03@gmail.com>
2022-10-19 20:40:11 +0530

    Refactor: UC4 Repeat till Player reaches winning position

--- src/com/snakeandladder/SnakeAndLadderProblem.java
+++ src/com/snakeandladder/SnakeAndLadderProblem.java
@@ -1,43 +1,69 @@
+//Repeat till winning position 100
 package com.snakeandladder;
 
 import java.util.Random;
 
 public class SnakeAndLadderProblem {
-
-
     public static void main(String[] args) {
-        int player_Position = 0;
-        int i = 0;
-        Random rand = new Random();
-        while (player_Position != 100)
-        {
-            ++i;
-            int diceRoll = rand.nextInt(1, 7);
-            int snakeLadder = rand.nextInt(1,3);
-            switch (snakeLadder)
-            {
-                case 1:
-                    System.out.println("\nGreat...! Player moved by :  " + diceRoll);
-                    player_Position += diceRoll;
-                    System.out.println("Player Position is: " + player_Position);
-                    break;
-                case 2:
-                    player_Position -= diceRoll;
-                    if (player_Position < 0)
-                    {
-                        player_Position = 0;
-                        System.out.println("\nOhh...! Player moved down to " +diceRoll);
-                        System.out.println("\nOhh...! Player has to restart from position: " + player_Position);
-                    }
-                    System.out.println("\nOhh...! Player moved down to " +diceRoll);
-                    System.out.println("Player position is: "+player_Position);
-                    break;
-                default:
-                    player_Position += diceRoll;
-                    System.out.println("Player position is: " + player_Position);
-                    break;
+        int playerPosition = 1;
+        int diceRoll = 0;
+        int SnakeArray[] = { 47, 62, 97 };
+        int LadderArray[] = { 9, 57, 20 };
+
+        System.out.println("Welcome to the Game of SNAKE & LADDER");
+        System.out.println("Starting Position of the Player is 0");
+
+        for (playerPosition = 1; playerPosition <= 100; playerPosition++) {
+
+            Random random = new Random();
+            diceRoll = (random.nextInt(6)+1);
+            System.out.println("Dice Number =" + " " + diceRoll);
+            playerPosition = playerPosition + diceRoll;
+            playerPosition = playerPosition - 1;
+
+            System.out.println("Current Position =" + " " + playerPosition);
+
+            if (playerPosition == 100) {
+                System.out.println("Player reaches winning position 100");
+            }
+            if (diceRoll == 0) {
+                playerPosition = playerPosition - diceRoll;
+                System.out.println("You have to stay on the same position!!");
+            }
+            if (playerPosition == SnakeArray[0]) {
+                playerPosition = 5;
+                System.out.println("OOPS...IT'S A SNAKE !");
+                System.out.println("You are at " + playerPosition + " position.");
+
+            } else if (playerPosition == SnakeArray[1]) {
+                playerPosition = 37;
+                System.out.println("OOPS...IT'S A SNAKE !");
+                System.out.println("You are at " + playerPosition + " position.");
+
+            } else if (playerPosition == SnakeArray[2]) {
+                playerPosition = 32;
+                System.out.println("OOPS...IT'S A SNAKE !");
+                System.out.println("You are at " + playerPosition + " position.");
+
+            } else if (playerPosition == LadderArray[0]) {
+                playerPosition = 44;
+                System.out.println("HURRAY...! YOU GOT A LADDER.");
+                System.out.println("You are at " + playerPosition + " position.");
+
+            } else if (playerPosition == LadderArray[1]) {
+                playerPosition = 79;
+                System.out.println("HURRAY...! YOU GOT A LADDER.");
+                System.out.println("You are at " + playerPosition + " position.");
+
+            } else if (playerPosition == LadderArray[2]) {
+                playerPosition = 98;
+                System.out.println("HURRAY...! YOU GOT A LADDER.");
+                System.out.println("You are at " + playerPosition + " position.");
+
+            } else if (playerPosition > 100) {
+                playerPosition = playerPosition - diceRoll;
+                System.out.println("YOU CAN'T JUMP, YOU MUST LAND ON A 100.");
             }
         }
-
     }
 }
